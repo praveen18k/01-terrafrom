0b979faaa92964ca1d1cd67cbba87b7780247d53
Praveen <praveenkothapally18@gmail.com>
2024-09-11 01:40:01 +0530

    vpc

--- 01-vpc-module/main.tf
+++ 01-vpc-module/main.tf
@@ -31,6 +31,38 @@ resource "aws_subnet" "private_subnet1" {
   tags = var.vpc_tags
 }
 
+#This will attach to internet gateway
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route"
+  }
+}
+# Elastic IP for NAT
+resource "aws_eip" "eip" {
+  domain = "vpc"
+}
+
+#NAT Gateway to connect internet from private subnets
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_subnet1
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.example]
+}
+##This will attach to NAT gateway
 resource "aws_route_table" "example" {
   vpc_id = aws_vpc.main.id
 
