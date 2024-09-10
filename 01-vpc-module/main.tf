26bcbe8d4e53899b38a919f2948fa1d1dd70ad6a
Praveen <praveenkothapally18@gmail.com>
2024-09-10 14:20:16 +0530

    vpc

--- 01-vpc-module/main.tf
+++ 01-vpc-module/main.tf
@@ -5,39 +5,3 @@ resource "aws_vpc" "main" {
 
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = var.vpc_tags
-}
-
-
-resource "aws_subnet" "public_subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet1
-
-  tags = var.vpc_tags
-}
-
-resource "aws_subnet" "private_subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet1
-
-  tags = var.vpc_tags
-}
-
-resource "aws_eip" "lb_eip" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.lb_eip.id
-  subnet_id     = aws_subnet.public_subnet1.id
-
-  tags = {
-    Name = "gw NAT"
-  }
-
-  depends_on = [aws_internet_gateway.igw]
-}
-
