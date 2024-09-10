b86b706cd61b5cfb06aeca47b117741d131f481b
Praveen <praveenkothapally18@gmail.com>
2024-09-11 00:47:51 +0530

    vpc

--- 01-vpc-module/main.tf
+++ 01-vpc-module/main.tf
@@ -14,7 +14,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "public_subnet1" {
-  count             = length(var.private_subnet1)
+  count             = length(var.public_subnet1)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet1[count.index]
   availability_zone = var.azs[count.index]
