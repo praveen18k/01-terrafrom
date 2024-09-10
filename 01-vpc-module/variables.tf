e55f6a496ce303095fac1aa8641b84e5db4518ce
Praveen <praveenkothapally18@gmail.com>
2024-09-10 10:55:39 +0530

    vpc

--- 01-vpc-module/variables.tf
+++ 01-vpc-module/variables.tf
@@ -2,3 +2,14 @@ variable "vpc" {
   default = "10.0.0.0/16"
 }
 
+variable "public_subnet1" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet1" {
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "vpc_tags" {
+  default = "my-vpc-module"
+}
