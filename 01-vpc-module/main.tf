8bade58457edb52c1caca456dffe8da2e6b7ae38
Praveen <praveenkothapally18@gmail.com>
2024-09-11 01:11:36 +0530

    vpc

--- 01-vpc-module/main.tf
+++ 01-vpc-module/main.tf
@@ -31,15 +31,19 @@ resource "aws_subnet" "private_subnet1" {
   tags = var.vpc_tags
 }
 
+#Route table resource
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = var.public_subnet1[count.index]
-    gateway_id = aws_internet_gateway.igw.id
-  }
 
   tags = {
     Name = "public-route"
   }
+}
+#Route for each public subnet CIDR block
+resource "aws_route" "public_route" {
+  for_each = var.public_subnet1 #iterate over each CIDR block
+
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = each.value # use the current CIDR block from the list
+  gateway_id             = aws_internet_gateway.igw.id
 }
