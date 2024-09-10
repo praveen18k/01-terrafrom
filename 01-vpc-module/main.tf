e324ca6cd933e685cb0d056371c716aa025e5066
Praveen <praveenkothapally18@gmail.com>
2024-09-11 01:49:00 +0530

    vpc

--- 01-vpc-module/main.tf
+++ 01-vpc-module/main.tf
@@ -52,7 +52,7 @@ resource "aws_eip" "eip" {
 #NAT Gateway to connect internet from private subnets
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.public_subnet1
+  subnet_id     = aws_subnet.public_subnet1[0].id
 
   tags = {
     Name = "gw NAT"
@@ -63,15 +63,15 @@ resource "aws_nat_gateway" "nat_gateway" {
   depends_on = [aws_internet_gateway.igw]
 }
 ##This will attach to NAT gateway
-resource "aws_route_table" "example" {
+resource "aws_route_table" "private_route" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
 
   tags = {
-    Name = "public-route"
+    Name = "private-route"
   }
 }
