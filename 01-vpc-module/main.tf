e7f9296cdd4077dd8ee37e1c1636d9397a14ec97
Praveen <praveenkothapally18@gmail.com>
2024-09-10 20:50:47 +0530

    vpc

--- 01-vpc-module/main.tf
+++ 01-vpc-module/main.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc
+  cidr_block           = var.vpc
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
 
   tags = var.vpc_tags
 
@@ -21,8 +23,10 @@ resource "aws_subnet" "public_subnet1" {
 }
 
 resource "aws_subnet" "private_subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet1
+  count             = length(var.private_subnet1)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet1
+  availability_zone = var.azs[count.index]
 
   tags = var.vpc_tags
 }
